f9cce6155b017703fd7b3aafe7358f571f284604
Skynet <nzureuz@gmail.com>
2023-10-04 16:02:43 -0600

    Update tagsinput.js

--- tagsinput.js
+++ tagsinput.js
@@ -291,8 +291,8 @@
 
       self.options = $.extend({}, defaultOptions, options);
       // When itemValue is set, freeInput should always be false
-      /*if (self.objectItems)
-        self.options.freeInput = false;*/
+      if (self.objectItems)
+        self.options.freeInput = false;
 
       makeOptionItemFunction(self.options, 'itemValue');
       makeOptionItemFunction(self.options, 'itemText');
